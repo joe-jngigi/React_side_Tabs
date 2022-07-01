faf373d5b5924ccb3c035abb2e836923fc291e3c
SoftOps <josephngigi775@gmail.com>
2022-07-01 16:03:06 +0300

    Tabs complete

--- src/App.js
+++ src/App.js
@@ -31,7 +31,21 @@ function App() {
       <div className="underline"></div>
       </div>
       <div className="jobs-center">
-        <button>Later</button>
+        <div className="btn-container">
+          {
+            jobs.map((item, index) =>{
+              return(
+                <button key={item.id} onClick = {
+                  () =>setValue(index)}
+                  className ={`job-btn ${
+                    index === value && 'active-btn'
+                  }`}
+                  >{item.company}
+                </button>
+              )
+            })
+          }
+        </div>
         {/**JOB INFO */}
         <article className="job-info">
           <h3>{title}</h3>
